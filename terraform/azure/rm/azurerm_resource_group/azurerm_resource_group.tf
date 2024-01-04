e9d2573e5a8996a875586f35e7a69db4512c9018
srijaallam <srija.allam10@gmail.com>
2024-01-04 16:33:37 -0500

    test

--- terraform/azure/rm/azurerm_resource_group/azurerm_resource_group.tf
+++ terraform/azure/rm/azurerm_resource_group/azurerm_resource_group.tf
@@ -1,11 +1,4 @@
 data "azurerm_resource_group" "resource_group" {
 
   name     = var.name
-  location = var.location
-
-  tags = var.tags
-}
-
-output "resource_group" {
-  value = azurerm_resource_group.resource_group
 }
